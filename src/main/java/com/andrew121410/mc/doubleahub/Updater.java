4c1fe5ba970850193ac5a0f23363d6c2150de86f
Andrew121410 <andrewcherring@gmail.com>
2023-01-03 16:44:38 -0500

    2023!!! (Bumped World1-6Utils)

--- src/main/java/com/andrew121410/mc/doubleahub/Updater.java
+++ src/main/java/com/andrew121410/mc/doubleahub/Updater.java
@@ -1,9 +1,9 @@
 package com.andrew121410.mc.doubleahub;
 
 
-import com.andrew121410.mc.world16utils.utils.ccutils.utils.AbstractBasicSelfUpdater;
+import com.andrew121410.mc.world16utils.utils.ccutils.utils.HashBasedUpdater;
 
-public class Updater extends AbstractBasicSelfUpdater {
+public class Updater extends HashBasedUpdater {
 
     private static final String JAR_URL = "https://github.com/Double-A-Network/DoubleA-Hub/releases/download/latest/DoubleA-Hub.jar";
     private static final String HASH_URL = "https://github.com/Double-A-Network/DoubleA-Hub/releases/download/latest/hash.txt";
